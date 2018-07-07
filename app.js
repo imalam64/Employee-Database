341a1ceb6505768ecded8552c2e331ee671d29f8
imalam64 <imalam64@gmail.com>
2018-07-07 12:40:13 -0700

    added database to append

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
          messagingSenderId: "883043883216"
      };
 firebase.initializeApp(config);
-     var database = firebase.database();
 
+var database = firebase.database();
 
 var name = '';
 var role = '';
@@ -21,27 +21,15 @@ var totalBilled = '';
 
 $('#submit').on('click', function(event){
 
-    event.preventDefault();
-    var name = $('#name-input').val().trim();
-    var role = $('#role-input').val().trim();
-    var startDate = $('#date-input').val().trim();
-    var payRate = $('#rate-input').val().trim();
+event.preventDefault();
+var name = $('#name-input').val().trim();
+var role = $('#role-input').val().trim();
+var startDate = $('#date-input').val().trim();
+var payRate = $('#rate-input').val().trim();
 
 
 timeWorked = ''; //startDate - dateStamp and convert to months
 totalBilled = timeWorked * payRate;
-
-    database.ref().set({
-        name: name,
-        role: role,
-        startDate: startDate,
-        payRate : payRate,
-        timeWorked : timeWorked,
-        total : totalBilled
-    });
-    
-
-});
 
 database.ref().push({
     name: name,
@@ -64,13 +52,25 @@ database.ref().on("value", function(snapshot) {
     console.log(snapshot.val().totalBilled);
 
     // Change the HTML to reflect
-    /* $("#name-display").text(snapshot.val().name);
-    $("#email-display").text(snapshot.val().email);
-    $("#age-display").text(snapshot.val().age);
-    $("#comment-display").text(snapshot.val().comment); */
+    var name = $('<td>');
+    var role = $('<td>');
+    var startDate = $('<td>');
+    var timeWorked = $('<td>');
+    var payRate = $('<td>');
+    var totalBilled = $('<td>');
+
+    name = snapshot.val().name;
+    role = snapshot.val().role;
+    startDate = snapshot.val().startDate;
+    timeWorked = snapshot.val().timeWorked;
+    payRate = snapshot.val().payRate;
+    totalBilled = snapshot.val().totalBilled;
+
+    $("#data-row").append(name, role, startDate, timeWorked, payRate, totalBilled);
 
     // Handle the errors
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
+});
 });
